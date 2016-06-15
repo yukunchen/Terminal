4f2a673ed5dc75e03f1b0eea793b822e87fc3ddf
Michael Niksa <miniksa@microsoft.com>
2016-06-15 09:20:21 -0700

    oops, semicolon missing. build error. fixing.

--- OpenConsole/ApiSorter.h
+++ OpenConsole/ApiSorter.h
@@ -3,4 +3,4 @@
 #include "IApiResponders.h"
 #include "DeviceProtocol.h"
 
-DWORD DoApiCall(_In_ DeviceProtocol* Server, _In_ IApiResponders* pResponders, _In_ CONSOLE_API_MSG* const pMsg)
+DWORD DoApiCall(_In_ DeviceProtocol* Server, _In_ IApiResponders* pResponders, _In_ CONSOLE_API_MSG* const pMsg);
