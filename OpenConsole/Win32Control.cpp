c2e0f884f1ef6ab9c7cf05f388a8fff12adbf8a2
Michael Niksa <miniksa@microsoft.com>
2016-06-20 16:15:38 -0700

    Implement remaining Win32Control calls.

--- OpenConsole/Win32Control.cpp
+++ OpenConsole/Win32Control.cpp
@@ -9,10 +9,11 @@ extern "C"
 
 typedef NTSTATUS(CALLBACK* PfnConsoleControl)(CONSOLECONTROL, PVOID, DWORD);
 
-NTSTATUS DoConsoleControl(CONSOLECONTROL Command, PVOID ConsoleInformation, DWORD ConsoleInformationLength)
+NTSTATUS CallConsoleControl(CONSOLECONTROL Command, PVOID ConsoleInformation, DWORD ConsoleInformationLength)
 {
 	NTSTATUS Status = STATUS_SUCCESS;
 
+	// TODO: cache this, use direct link, something.
 	HMODULE hModule = LoadLibraryW(L"user32.dll");
 	if (hModule == nullptr)
 	{
@@ -44,5 +45,43 @@ DWORD Win32Control::NotifyConsoleTypeApplication(_In_ HANDLE const ProcessHandle
 	CONSOLE_PROCESS_INFO cpi;
 	cpi.dwProcessID = HandleToUlong(ProcessHandle);
 	cpi.dwFlags = CPI_NEWPROCESSWINDOW;
-	return DoConsoleControl(ConsoleNotifyConsoleApplication, &cpi, sizeof(cpi));
+	return CallConsoleControl(ConsoleNotifyConsoleApplication, &cpi, sizeof(cpi));
+}
+
+DWORD Win32Control::NotifyWindowOwner(_In_ HANDLE const ProcessHandle, _In_ HANDLE const ThreadHandle, _In_ HWND const WindowHandle)
+{
+	CONSOLEWINDOWOWNER cwo;
+	cwo.ProcessId = HandleToULong(ProcessHandle);
+	cwo.ThreadId = HandleToULong(ThreadHandle);
+	cwo.hwnd = WindowHandle;
+	return CallConsoleControl(ConsoleSetWindowOwner, &cwo, sizeof(cwo));
+}
+
+DWORD Win32Control::NotifyForegroundProcess(_In_ HANDLE const ProcessHandle, _In_ BOOL IsForeground)
+{
+	CONSOLESETFOREGROUND csf;
+	csf.hProcess = ProcessHandle;
+	csf.bForeground = IsForeground;
+	return CallConsoleControl(ConsoleSetForeground, &csf, sizeof(csf));
+}
+
+DWORD Win32Control::NotifyCaretPosition(_In_ HWND const WindowHandle, _In_ RECT const CursorPixelsInClientArea)
+{
+	CONSOLE_CARET_INFO cci;
+	cci.hwnd = WindowHandle;
+	cci.rc = CursorPixelsInClientArea;
+	return CallConsoleControl(ConsoleSetCaretInfo, &cci, sizeof(cci));
+}
+
+DWORD Win32Control::NotifyEndTask(_In_ HANDLE const ProcessHandle,
+								  _In_ HWND const WindowHandle,
+								  _In_ ULONG const EventType,
+								  _In_ ULONG const EventFlags)
+{
+	CONSOLEENDTASK cet;
+	cet.ProcessId = ProcessHandle;
+	cet.hwnd = WindowHandle;
+	cet.ConsoleEventCode = EventType;
+	cet.ConsoleFlags = EventFlags;
+	return CallConsoleControl(ConsoleEndTask, &cet, sizeof(cet));
 }
--- OpenConsole/Win32Control.h
+++ OpenConsole/Win32Control.h
@@ -2,5 +2,9 @@
 namespace Win32Control
 {
 	DWORD NotifyConsoleTypeApplication(_In_ HANDLE const ProcessHandle);
+	DWORD NotifyWindowOwner(_In_ HANDLE const ProcessHandle, _In_ HANDLE const ThreadHandle, _In_ HWND const WindowHandle);
+	DWORD NotifyForegroundProcess(_In_ HANDLE const ProcessHandle, _In_ BOOL IsForeground);
+	DWORD NotifyCaretPosition(_In_ HWND const WindowHandle, _In_ RECT const CursorPixelsInClientArea);
+	DWORD NotifyEndTask(_In_ HANDLE const ProcessHandle, _In_ HWND const WindowHandle, _In_ ULONG const EventType, _In_ ULONG const EventFlags);
 };
 
