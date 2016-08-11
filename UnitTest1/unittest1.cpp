894d221f7ff3e9ac39425de4d252f2f211db2b0d
Michael Niksa <miniksa@microsoft.com>
2016-08-11 15:51:28 -0700

    Remove TAEF test. Try MSTest/VSTest empty file in XES.

---
+++ UnitTest1/unittest1.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        
+        TEST_METHOD(TestMethod1)
+        {
+            Assert::IsTrue(true);
+        }
+    };
+}
