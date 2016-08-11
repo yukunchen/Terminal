70e27d59ec1cc8e93282ec7d5a6371d199a5c809
Michael Niksa <miniksa@microsoft.com>
2016-08-11 15:32:07 -0700

    Test TAEF in XES.

---
+++ TaefCppProject1/SampleTest.cpp
@@ -0,0 +1,26 @@
+/*
+* Copyright (c) Microsoft Corporation. All rights reserved.
+*
+* This File was generated using the VisualTAEF C++ Project Wizard.
+* Class Name: SampleTest
+*/
+#include "StdAfx.h"
+#include <WexTestClass.h>
+
+using namespace WEX::Logging;
+using namespace WEX::TestExecution;
+
+namespace 
+{
+    class SampleTest
+    {
+    public:
+        // Declare this class as a TestClass, and supply metadata if necessary.
+        TEST_CLASS(SampleTest);
+
+        TEST_METHOD(TestClass1Test1)
+        {
+            VERIFY_IS_TRUE(true);
+        }
+    };
+} /* namespace  */ 
