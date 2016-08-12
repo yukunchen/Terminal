3d95db08fa2cafa36416819cf2b6c3a709c1ae9b
Michael Niksa <miniksa@microsoft.com>
2016-08-12 10:01:05 -0700

    Try with TAEF.

---
+++ TaefCppProject2/TestClass1.cpp
@@ -0,0 +1,23 @@
+/*
+* Copyright (c) Microsoft Corporation. All rights reserved.
+*
+* This File was generated using the VisualTAEF C++ Project Wizard.
+* Class Name: TestClass1
+*/
+#include "StdAfx.h"
+#include <WexTestClass.h>
+
+using namespace WEX::Logging;
+using namespace WEX::TestExecution;
+
+namespace TaefCppProject2
+{
+    class TestClass1
+    {
+        TEST_CLASS(TestClass1);
+
+        TEST_METHOD(TestClass1Test1)
+        {
+        }
+    };
+} /* namespace TaefCppProject2 */ 
