7882ea25c78661f0b582e17c862a5f8107831554
Michael Niksa <miniksa@microsoft.com>
2016-09-12 12:45:05 -0700

    Create EXE project type for OpenConsole.

---
+++ src/host/exe/exemain.cpp
@@ -0,0 +1,10 @@
+#include "precomp.h"
+
+int CALLBACK WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nCmdShow)
+{
+    return S_OK;
+}
