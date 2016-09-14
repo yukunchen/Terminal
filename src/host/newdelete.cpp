3c5969601730a9297182affa5bfc2fce26779d6e
Austin Diviness <austdi@microsoft.com>
2016-09-14 10:36:35 -0700

    updated suppression text on new and delete

--- src/host/newdelete.cpp
+++ src/host/newdelete.cpp
@@ -14,25 +14,25 @@ void EnsureHeap()
     return;
 }
 
-#pragma prefast(suppress: 28250, "we don't care that the original compiler internal implementation didn't have SAL annotations")
+#pragma prefast(suppress: 28250, "ignoring this warning because the original compiler internal implementation didn't have SAL annotations")
 void * _cdecl operator new(_In_ size_t size)
 {
     return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size);
 }
 
-#pragma prefast(suppress: 28250, "we don't care that the original compiler internal implementation didn't have SAL annotations")
+#pragma prefast(suppress: 28250, "ignoring this warning because the original compiler internal implementation didn't have SAL annotations")
 void * __cdecl operator new[](_In_ size_t cb)
 {
     return operator new(cb);
 }
 
-#pragma prefast(suppress: 28250, "we don't care that the original compiler internal implementation didn't have SAL annotations")
+#pragma prefast(suppress: 28250, "ignoring this warning because the original compiler internal implementation didn't have SAL annotations")
 void _cdecl operator delete(_In_ void *pv)
 {
     HeapFree(GetProcessHeap(), 0, pv);
 }
 
-#pragma prefast(suppress: 28250, "we don't care that the original compiler internal implementation didn't have SAL annotations")
+#pragma prefast(suppress: 28250, "ignoring this warning because the original compiler internal implementation didn't have SAL annotations")
 void __cdecl operator delete[](_In_ void * p)
 {
     operator delete(p);
