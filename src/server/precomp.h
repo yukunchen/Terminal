fc46afca3835be7862d8ec72291373517129481d
Michael Niksa <miniksa@microsoft.com>
2016-10-10 15:56:09 -0700

    Initial project creation for server library.

---
+++ src/server/precomp.h
@@ -0,0 +1,11 @@
+/*++
+Copyright (c) Microsoft Corporation
+
+Module Name:
+- precomp.h
+
+Abstract:
+- Contains external headers to include in the precompile phase of console build process.
+- Avoid including internal project headers. Instead include them only in the classes that need them (helps with test project building).
+--*/
+
