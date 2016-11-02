cbcb00c79cb9671c472089ea542f2cb19b62b126
Austin Diviness <austdi@microsoft.com>
2016-11-02 15:56:32 -0700

    added Message ft tests to runft.bat

--- tools/runft.bat
+++ tools/runft.bat
@@ -4,7 +4,8 @@
     rem %OPENCON%\bin\%ARCH%\Debug\ConHost.CJK.Tests.dll ^
     %OPENCON%\bin\%ARCH%\Debug\ConHost.Api.Tests.dll ^
     %OPENCON%\bin\%ARCH%\Debug\ConHost.Resize.Tests.dll ^
+    %OPENCON%\bin\%ARCH%\Debug\ConHost.Message.Tests.dll ^
     %*
 
-@rem the CJK tests are failing currently, so I'm temporarily disabling those 
+@rem the CJK tests are failing currently, so I'm temporarily disabling those
 @rem    (while we wait for the improved utf8 support)
