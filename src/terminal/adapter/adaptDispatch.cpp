edd9926dc3fc7358151397acd143a622233ea138
Mike Griese <migrie@microsoft.com>
2016-12-06 15:29:55 -0800

    nit: newline

--- src/terminal/adapter/adaptDispatch.cpp
+++ src/terminal/adapter/adaptDispatch.cpp
@@ -1262,7 +1262,8 @@ bool AdaptDispatch::SetTopBottomScrollingMargins(_In_ SHORT const sTopMargin, _I
         {
             fSuccess = false;
         }
-        else if (sActualTop == 0 && sActualBottom == sScreenHeight) {
+        else if (sActualTop == 0 && sActualBottom == sScreenHeight)
+        {
             // Client requests setting margins to the entire screen 
             //    - clear them instead of setting them.
             // This is for apps like `apt` (NOT `apt-get` which set scroll 
