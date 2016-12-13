12a0f889c5e9c09eef7e642538888279542eed0e
Michael Niksa <miniksa@microsoft.com>
2016-12-13 15:00:07 -0800

    Add no-op test for x86 so build env doesn't complain it has no tests.

---
+++ src/tools/ft_x86_noop/Test.cpp
@@ -0,0 +1,19 @@
+﻿/********************************************************
+*                                                       *
+*   Copyright (C) Microsoft. All rights reserved.       *
+*                                                       *
+********************************************************/
+
+#include "precomp.h"
+
+class NoOpTests
+{
+    TEST_CLASS(NoOpTests);
+
+    TEST_METHOD(TestNoOp);
+};
+
+void NoOpTests::TestNoOp()
+{
+    VERIFY_IS_TRUE(true);
+}
