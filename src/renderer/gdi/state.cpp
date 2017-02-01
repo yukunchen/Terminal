cd68fc1c8875a5ca3060609daca3dee21211a662
Paul Campbell <paulcam@microsoft.com>
2017-02-01 10:49:25 -0800

    Clean up comments around choice of DEFAULT_CHARSET.

--- src/renderer/gdi/state.cpp
+++ src/renderer/gdi/state.cpp
@@ -295,32 +295,18 @@ HRESULT GdiEngine::_GetProposedFont(_In_ FontInfoDesired const * const pfiFontDe
     lf.lfHeight = s_ScaleByDpi(coordFontRequested.Y, iDpi);
     lf.lfWidth = s_ScaleByDpi(coordFontRequested.X, iDpi);
     lf.lfWeight = pfiFontDesired->GetWeight();
-
-    // If we're searching for Terminal, our supported Raster Font, then we must use OEM_CHARSET.
-    // If the System's Non-Unicode Setting is set to English (United States) which is 437 
-    // and we try to enumerate Terminal with the console codepage as 932, that will turn into SHIFTJIS_CHARSET.
-    // Despite C:\windows\fonts\vga932.fon always being present, GDI will refuse to load the Terminal font 
-    // that doesn't correspond to the current System Non-Unicode Setting. It will then fall back to a TrueType
-    // font that does support the SHIFTJIS_CHARSET (because Terminal with CP 437 a.k.a. C:\windows\fonts\vgaoem.fon does NOT support it.)
-    // This is OK for display purposes (things will render properly) but not OK for API purposes.
-    // Because the API is affected by the raster/TT status of the actively selected font, we can't have
-    // GDI choosing a TT font for us when we ask for Raster. We have to settle for forcing the current system
-    // Terminal font to load even if it doesn't have the glyphs necessary such that the APIs continue to work fine.
-
-    lf.lfCharSet = DEFAULT_CHARSET;
-    // const UINT uiOEMCP = GetOEMCP();
-    // if (!IS_ANY_DBCS_CODEPAGE(uiOEMCP) &&
-    //     uiOEMCP != pfiFontDesired->GetCodePage() &&
-    //     0 == wcscmp(pfiFontDesired->GetFaceName(), L"Terminal"))
-    // {
-    //     lf.lfCharSet = OEM_CHARSET;
-    // }
-    // else
-    // {
-    //     lf.lfCharSet = pfiFontDesired->GetCharSet();
-    // }
-
     lf.lfQuality = DRAFT_QUALITY;
+
+    // If we're searching for Terminal, our supported Raster Font, then we must use OEM_CHARSET. If the System's
+    // Non-Unicode Setting is set to English (United States) which is 437 and we try to enumerate Terminal with the
+    // console codepage as 932, that will turn into SHIFTJIS_CHARSET. Despite C:\windows\fonts\vga932.fon always being
+    // present, GDI will refuse to load the Terminal font that doesn't correspond to the current System Non-Unicode
+    // Setting. It will then fall back to a TrueType font that does support the SHIFTJIS_CHARSET (because Terminal with
+    // CP 437 a.k.a. C:\windows\fonts\vgaoem.fon does NOT support it.) This is OK for display purposes (things will
+    // render properly) but not OK for API purposes. Because the API is affected by the raster/TT status of the actively
+    // selected font, we can't have GDI choosing a TT font for us when we ask for Raster. Specifying DEFAULT_CHARSET
+    // will give us the behavior we want in all cases.
+    lf.lfCharSet = DEFAULT_CHARSET;
 
     // NOTE: not using what GDI gave us because some fonts don't quite roundtrip (e.g. MS Gothic and VL Gothic)
     lf.lfPitchAndFamily = (FIXED_PITCH | FF_MODERN);
