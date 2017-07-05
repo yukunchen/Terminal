df09a89ecae3a460b5c767e18faef5e5299ebab2
Michael Niksa <miniksa@microsoft.com>
2017-07-05 15:44:05 -0700

    Raw WriteFile test.

--- src/host/ft_host/api/FileTests.cpp
+++ src/host/ft_host/api/FileTests.cpp
@@ -14,13 +14,24 @@ class FileTests
 {
     TEST_CLASS(FileTests);
 
+    TEST_METHOD_SETUP(FileTestSetup);
+    TEST_METHOD_CLEANUP(FileTestCleanup);
+
     TEST_METHOD(TestUtf8WriteFileInvalid);
 
-    BEGIN_TEST_METHOD(TestWriteFile)
-
-    END_TEST_METHOD()
+    TEST_METHOD(TestWriteFileRaw);
 
 };
+
+bool FileTests::FileTestSetup()
+{
+    return true;
+}
+
+bool FileTests::FileTestCleanup()
+{
+    return true;
+}
 
 void FileTests::TestUtf8WriteFileInvalid()
 {
@@ -72,7 +83,47 @@ void FileTests::TestUtf8WriteFileInvalid()
     VERIFY_ARE_EQUAL(dwExpectedWritten, dwWritten);
 }
 
-void FileTests::TestWriteFile()
+void FileTests::TestWriteFileRaw()
 {
+    PCSTR strTest = "a\x7s\x8d\x9f\xag\xdh";
+    DWORD cchTest = (DWORD)strlen(strTest);
+    String strReadBackExpected(strTest);
 
+    HANDLE hOut = GetStdOutputHandle();
+    VERIFY_IS_NOT_NULL(hOut, L"Verify we have the standard output handle.");
+
+    CONSOLE_SCREEN_BUFFER_INFOEX csbiexBefore = { 0 };
+    csbiexBefore.cbSize = sizeof(csbiexBefore);
+    VERIFY_WIN32_BOOL_SUCCEEDED(GetConsoleScreenBufferInfoEx(hOut, &csbiexBefore), L"Retrieve screen buffer properties before writing.");
+
+    VERIFY_WIN32_BOOL_SUCCEEDED(SetConsoleMode(hOut, 0), L"Set raw write mode.");
+
+    COORD const coordZero = { 0 };
+    VERIFY_ARE_EQUAL(coordZero, csbiexBefore.dwCursorPosition, L"Cursor should be at 0,0 in fresh buffer.");
+
+    DWORD dwWritten = 0;
+    VERIFY_WIN32_BOOL_SUCCEEDED(WriteFile(hOut, strTest, cchTest, &dwWritten, nullptr), L"Write text into buffer using WriteFile");
+    VERIFY_ARE_EQUAL(cchTest, dwWritten);
+
+    CONSOLE_SCREEN_BUFFER_INFOEX csbiexAfter = { 0 };
+    csbiexAfter.cbSize = sizeof(csbiexAfter);
+    VERIFY_WIN32_BOOL_SUCCEEDED(GetConsoleScreenBufferInfoEx(hOut, &csbiexAfter), L"Retrieve screen buffer properties after writing.");
+
+    csbiexBefore.dwCursorPosition.X += (SHORT)cchTest;
+    VERIFY_ARE_EQUAL(csbiexBefore.dwCursorPosition, csbiexAfter.dwCursorPosition, L"Verify cursor moved expected number of squares for the write length.");
+
+    DWORD cbReadBackBuffer = cchTest + 2; // +1 so we can read back a "space" that should be after what we wrote. +1 more so this can be null terminated for String class comparison.
+    wistd::unique_ptr<char[]> strReadBack = wil::make_unique_nothrow<char[]>(cbReadBackBuffer);
+    VERIFY_IS_NOT_NULL(strReadBack, L"Verify allocated buffer for string readback isn't null.");
+    ZeroMemory(strReadBack.get(), cbReadBackBuffer * sizeof(char));
+
+    DWORD dwRead = 0;
+    VERIFY_WIN32_BOOL_SUCCEEDED(ReadConsoleOutputCharacterA(hOut, strReadBack.get(), cchTest + 1, coordZero, &dwRead), L"Read back the data in the buffer.");
+    // +1 to read back the space that should be after the text we wrote
+
+    strReadBackExpected += " "; // add in the space that should appear after the written text (buffer should be space filled when empty)
+
+    VERIFY_ARE_EQUAL(strReadBackExpected, String(strReadBack.get()), L"Ensure that the buffer contents match what we expected based on what we wrote.");
+
+    Sleep(2000);
 }
