0d4b3b5d22cd12361b7354c2996e3e2e74cbdb50
Mike Griese <migrie@microsoft.com>
2017-10-06 13:01:36 -0700

    Revert "Fix the VS Build after the razzle build changes"
    This reverts commit 5ab7f8d3fb1ab4ce3636c1f41d7901a965b2b8f6.

--- src/host/exe/exemain.cpp
+++ src/host/exe/exemain.cpp
@@ -8,10 +8,6 @@
 
 #include "..\Entrypoints.h"
 #include <string>
-
-
-// This first main is the VS entrypoint, the second is the razzle entrypoint.
-
 // Routine Description:
 // - Main entry point for EXE version of console launching.
 //   This can be used as a debugging/diagnostics tool as well as a method of testing the console without
@@ -23,19 +19,24 @@
 // - nCmdShow - Unused variable specifying window show/hide state for Win32 mode applications.
 // Return value:
 // - [[noreturn]] - This function will not return. It will kill the thread we were called from and the console server threads will take over.
-int CALLBACK wWinMain(
-    _In_ HINSTANCE /*hInstance*/,
-    _In_ HINSTANCE /*hPrevInstance*/,
-    _In_ PWSTR pwszCmdLine,
-    _In_ int /*nCmdShow*/)
-{
-    Entrypoints::StartConsoleForCmdLine(pwszCmdLine);
-}
+// int CALLBACK wWinMain(
+//     _In_ HINSTANCE /*hInstance*/,
+//     _In_ HINSTANCE /*hPrevInstance*/,
+//     _In_ PWSTR pwszCmdLine,
+//     _In_ int /*nCmdShow*/)
+// {
+//     Entrypoints::StartConsoleForCmdLine(pwszCmdLine);
+// }
 
 
-int WINAPI wmain(int argc, wchar_t* argv[], wchar_t* envp[])
+
+int WINAPI wmain( int argc, wchar_t *argv[ ], wchar_t *envp[ ] )
+// int WINAPI wmain(__in HINSTANCE /*hInstance*/, __in_opt HINSTANCE /*hPrevInstance*/, __in LPSTR /*pwcCmdLine*/, __in int /*iCmdShow*/)
 {
-    UNREFERENCED_PARAMETER(envp);
+    envp;
+    // wchar_t* cmdline = GetCommandLineW();
+    // wchar_t* realCmd = cmdline;
+    // while(*realCmd != nullptr)
     std::wstring args = L"";
     for (auto i = 1; i < argc; i++)
     {
