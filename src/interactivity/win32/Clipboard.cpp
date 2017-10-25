b05e65275f49e61737d107152d3daa9483017105
Austin Diviness <austdi@microsoft.com>
2017-10-25 13:42:54 -0700

    disallow alt+numpad conversion for utf8 codepage on clipboard paste

--- src/interactivity/win32/Clipboard.cpp
+++ src/interactivity/win32/Clipboard.cpp
@@ -270,59 +270,70 @@ std::deque<std::unique_ptr<KeyEvent>> Clipboard::CharToKeyboardEvents(_In_ const
 // - will throw exception on error
 std::deque<std::unique_ptr<KeyEvent>> Clipboard::CharToNumpad(_In_ const wchar_t wch)
 {
-    const UINT codepage = ServiceLocator::LocateGlobals()->getConsoleInformation()->OutputCP;
-    const int radix = 10;
-
     std::deque<std::unique_ptr<KeyEvent>> keyEvents;
 
-    std::wstring wstr{ wch };
-    std::deque<char> convertedChars;
-    convertedChars = ConvertToOem(codepage, wstr);
+    //alt keydown
+    keyEvents.push_back(std::make_unique<KeyEvent>(TRUE,
+                                                    1ui16,
+                                                    static_cast<WORD>(VK_MENU),
+                                                    altScanCode,
+                                                    UNICODE_NULL,
+                                                    LEFT_ALT_PRESSED));
 
-    for (char ch : convertedChars)
+    const UINT codepage = ServiceLocator::LocateGlobals()->getConsoleInformation()->OutputCP;
+    if (codepage != CP_UTF8)
     {
-        // char values are in the range [-128, 127] so we need to be
-        // able to store up to 4 chars from the conversion
-        char charString[4] = { 0 };
-        THROW_HR_IF(E_INVALIDARG, 0 != _itoa_s(ch, charString, ARRAYSIZE(charString), radix));
+        const int radix = 10;
 
-        keyEvents.push_back(std::make_unique<KeyEvent>(TRUE,
-                                                       1ui16,
-                                                       static_cast<WORD>(VK_MENU),
-                                                       altScanCode,
-                                                       UNICODE_NULL,
-                                                       LEFT_ALT_PRESSED));
-        for (size_t i = 0; i < ARRAYSIZE(charString); ++i)
+        std::wstring wstr{ wch };
+        std::deque<char> convertedChars;
+        convertedChars = ConvertToOem(codepage, wstr);
+
+        assert(convertedChars.size() <= 1);
+
+        for (char ch : convertedChars)
         {
-            if (charString[i] == 0)
-            {
-                break;
-            }
-            const WORD virtualKey = charString[i] + '0' + VK_NUMPAD0;
-            const WORD virtualScanCode = static_cast<WORD>(MapVirtualKeyW(virtualKey, MAPVK_VK_TO_VSC));
+            unsigned char uch = static_cast<unsigned char>(ch);
+            // unsigned char values are in the range [0, 255] so we need to be
+            // able to store up to 4 chars from the conversion (including the end of string char)
+            char charString[4] = { 0 };
+            THROW_HR_IF(E_INVALIDARG, 0 != _itoa_s(uch, charString, ARRAYSIZE(charString), radix));
 
-            keyEvents.push_back(std::make_unique<KeyEvent>(TRUE,
-                                                           1ui16,
-                                                           virtualKey,
-                                                           virtualScanCode,
-                                                           UNICODE_NULL,
-                                                           LEFT_ALT_PRESSED));
-            keyEvents.push_back(std::make_unique<KeyEvent>(FALSE,
-                                                           1ui16,
-                                                           virtualKey,
-                                                           virtualScanCode,
-                                                           UNICODE_NULL,
-                                                           LEFT_ALT_PRESSED));
+            for (size_t i = 0; i < ARRAYSIZE(charString); ++i)
+            {
+                if (charString[i] == 0)
+                {
+                    break;
+                }
+                const WORD virtualKey = charString[i] - '0' + VK_NUMPAD0;
+                const WORD virtualScanCode = static_cast<WORD>(MapVirtualKeyW(virtualKey, MAPVK_VK_TO_VSC));
+
+                keyEvents.push_back(std::make_unique<KeyEvent>(TRUE,
+                                                               1ui16,
+                                                               virtualKey,
+                                                               virtualScanCode,
+                                                               UNICODE_NULL,
+                                                               LEFT_ALT_PRESSED));
+                keyEvents.push_back(std::make_unique<KeyEvent>(FALSE,
+                                                               1ui16,
+                                                               virtualKey,
+                                                               virtualScanCode,
+                                                               UNICODE_NULL,
+                                                               LEFT_ALT_PRESSED));
+            }
         }
-        keyEvents.push_back(std::make_unique<KeyEvent>(FALSE,
-                                                       1ui16,
-                                                       static_cast<WORD>(VK_MENU),
-                                                       altScanCode,
-                                                       wch,
-                                                       0));
     }
+
+    // alt keyup
+    keyEvents.push_back(std::make_unique<KeyEvent>(FALSE,
+                                                    1ui16,
+                                                    static_cast<WORD>(VK_MENU),
+                                                    altScanCode,
+                                                    wch,
+                                                    0));
     return keyEvents;
 }
+
 
 // Routine Description:
 // - Copies the selected area onto the global system clipboard.
