381bb0768aec69325941d1f192a5e8dcc9b002eb
Michael Niksa <miniksa@microsoft.com>
2017-11-20 16:51:54 +0000

    Merged PR 1120770: incorrect length given to InsertAttrRuns function =
    incorrect length given to InsertAttrRuns function (recently updated to improve performance.) #13505675
    
    I had updated the InsertAttrRuns function to use the inplace algorithm for improved performance and added this assert while I was at it to try to catch bad usages. Turns out there was a bad usage in the IME path. This corrects the values given to the function. (Off by 1 error).
    
    Related work items: #13505675

--- src/host/convarea.cpp
+++ src/host/convarea.cpp
@@ -797,7 +797,7 @@ void StreamWriteToScreenBufferIME(_In_reads_(StringLength) PWCHAR String,
                 Row->AttrRow.InsertAttrRuns(&InsertedRun,
                                             1,
                                             TargetPoint.X + i,
-                                            (SHORT)(TargetPoint.X + i + 1),
+                                            (SHORT)(TargetPoint.X + i),
                                             coordScreenBufferSize.X);
             }
         }
