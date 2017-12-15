99d313ebada99f9c1e9ef0375c195569f620cca0
Austin Diviness <austdi@microsoft.com>
2017-12-15 13:33:52 -0800

    properly suppress unreachable code warning since it needs to be done at the function level

--- src/host/textBuffer.cpp
+++ src/host/textBuffer.cpp
@@ -433,13 +433,14 @@ void TextAttributeRun::SetAttributes(_In_ const TextAttribute textAttribute)
 // - pAttr - The default text attributes to use on text in this row.
 // Return Value:
 // - <none>
+// Note:
+// - We suppress C4702 (unreachable code) because we really do want to have the try/catch block around calls
+// to wil::make_unique_nothrow even though currently the catch block can't be reached. We want to guard
+// against the possibility of TextAttributeRun's constructor changing from the default one to one that may
+// throw.
+#pragma warning(suppress:4702)
 bool ATTR_ROW::Initialize(_In_ UINT const cchRowWidth, _In_ const TextAttribute attr)
 {
-    // Disable C4702 (unreacable code) because we really do want to have the try/catch block here even if
-    // currently the catch block can't be reached. We want to guard against the possibility of
-    // TextAttributeRun's constructor changing from the default one to one that may throw.
-#pragma warning(push,4)
-#pragma warning(disable:4702)
     wistd::unique_ptr<TextAttributeRun[]> pNewRun;
     try
     {
@@ -449,7 +450,6 @@ bool ATTR_ROW::Initialize(_In_ UINT const cchRowWidth, _In_ const TextAttribute 
     {
         return false;
     }
-#pragma warning(pop)
 
     bool fSuccess = pNewRun != nullptr;
     if (fSuccess)
@@ -733,6 +733,12 @@ size_t _DebugGetTotalLength(_In_reads_(cRun) const TextAttributeRun* const rgRun
 // Return Value:
 // - STATUS_NO_MEMORY if there wasn't enough memory to insert the runs
 //   otherwise STATUS_SUCCESS if we were successful.
+// Note:
+// - We suppress C4702 (unreachable code) because we really do want to have the try/catch block around calls
+// to wil::make_unique_nothrow even though currently the catch block can't be reached. We want to guard
+// against the possibility of TextAttributeRun's constructor changing from the default one to one that may
+// throw.
+#pragma warning(suppress:4702)
 HRESULT ATTR_ROW::InsertAttrRuns(_In_reads_(cAttrs) const TextAttributeRun* const rgInsertAttrs,
                                   _In_ const UINT cInsertAttrs,
                                   _In_ const UINT iStart,
@@ -798,18 +804,12 @@ HRESULT ATTR_ROW::InsertAttrRuns(_In_reads_(cAttrs) const TextAttributeRun* cons
     // fact that an existing piece of the run was split in half (to hold the latter half).
     UINT const cNewRun = _cList + cInsertAttrs + 1;
 
-    // Disable C4702 (unreacable code) because we really do want to have the try/catch block here even if
-    // currently the catch block can't be reached. We want to guard against the possibility of
-    // TextAttributeRun's constructor changing from the default one to one that may throw.
-#pragma warning(push,4)
-#pragma warning(disable:4702)
     wistd::unique_ptr<TextAttributeRun[]> pNewRun;
     try
     {
         pNewRun = wil::make_unique_nothrow<TextAttributeRun[]>(cNewRun);
     }
     CATCH_RETURN();
-#pragma warning(pop)
 
     RETURN_IF_NULL_ALLOC(pNewRun);
 
