5e78addac62fb6c7699e4478976a2e4fae1d7011
Mike Griese (v2) <migrie@microsoft.com>
2018-01-10 23:26:46 +0000

    Merged PR 1267085: Merge dev/migrie/r/13994975-refactor-cmdline to dev/main
    Pull out the alias and history code into their own files.
    
    I've been meaning to do this for a while, but never got around to doing it the last time I was working on aliases. This will make working in those areas of the codebase, as well as cmdline in general, a lot easier.
    
    13994975 is related, but this does not fix that.
    
    Related work items: #13994975

---
+++ src/host/alias.h
@@ -0,0 +1,43 @@
+/*++
+Copyright (c) Microsoft Corporation
+
+Module Name:
+- alias.h
+
+Abstract:
+- Encapsulates the cmdline functions and structures specifically related to 
+        command alias functionality.
+--*/
+#pragma once
+
+//
+// aliases are grouped per console, per exe.
+//
+
+typedef struct _ALIAS
+{
+    LIST_ENTRY ListLink;
+    USHORT SourceLength;
+    USHORT TargetLength;
+    _Field_size_bytes_opt_(SourceLength) PWCHAR Source;
+    _Field_size_bytes_(TargetLength) PWCHAR Target;
+} ALIAS, *PALIAS;
+
+typedef struct _EXE_ALIAS_LIST
+{
+    LIST_ENTRY ListLink;
+    USHORT ExeLength;
+    _Field_size_bytes_opt_(ExeLength) PWCHAR ExeName;
+    LIST_ENTRY AliasList;
+} EXE_ALIAS_LIST, *PEXE_ALIAS_LIST;
+
+NTSTATUS MatchAndCopyAlias(_In_reads_bytes_(cbSource) PWCHAR pwchSource,
+                           _In_ USHORT cbSource,
+                           _Out_writes_bytes_(*pcbTarget) PWCHAR pwchTarget,
+                           _Inout_ PUSHORT pcbTarget,
+                           _In_reads_bytes_(cbExe) PWCHAR pwchExe,
+                           _In_ USHORT cbExe,
+                           _Out_ PDWORD pcLines);
+
+void ClearCmdExeAliases();
+void FreeAliasBuffers();
