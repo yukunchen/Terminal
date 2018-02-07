b339fc81470b988e6608dad4b66ae086f9161ded
Mike Griese (v2) <migrie@microsoft.com>
2018-02-07 21:18:42 +0000

    Merged PR 1416735: Merge dev/migrie/b/15813316-inherit-cursor to dev/main
    This is the bulk of the work remaining to get interop via conpty working.
    
    Primarily, this adds a new argument to conhost, `--inheritcursor`. When a terminal uses that arg with conhost, conhost will emit a VT sequence on startup to try and get the current cursor position of the end terminal. The terminal (for ex, tmux, or conhost if there's nothing else running as a pty master) will respond with the position of the cursor on the input pipe, which conhost will read, and then move the cursor to that position. Conhost will then continue on it's merry way of starting up.
    
    All of this happens *before* the client application is started, so client's output will start in the inherited location in the conpty.
    
    We'll also *not* paint the region of the buffer above the inherited cursor position, until the contents of the conpty naturally get there. This is to prevent conpty from painting the empty region of the buffer to the terminal.
    
    See also MSFT: 15823972 and [OS PR:1412191](https://microsoft.visualstudio.com/_git/os/pullrequest/1412191) for the WSL half of this change.
    
    CC @<benhill@ntdev.microsoft.com> @<jostarks@ntdev.microsoft.com>
    
    Once this PR is in, it'll be checked into ..._ACI_OSS, then cherry-picked to the feature/low_rs4 branch for staging.
    
    Cherry picked from !1412298
    
    Related work items: #15813316

--- src/renderer/vt/invalidate.cpp
+++ src/renderer/vt/invalidate.cpp
@@ -14,11 +14,11 @@ using namespace Microsoft::Console::Types;
 using namespace Microsoft::Console::Render;
 
 // Routine Description:
-// - Notifies us that the system has requested a particular pixel area of the 
+// - Notifies us that the system has requested a particular pixel area of the
 //      client rectangle should be redrawn. (On WM_PAINT)
 //  For VT, this doesn't mean anything. So do nothing.
 // Arguments:
-// - prcDirtyClient - Pointer to pixel area (RECT) of client region the system 
+// - prcDirtyClient - Pointer to pixel area (RECT) of client region the system
 //      believes is dirty
 // Return Value:
 // - S_OK
@@ -39,7 +39,7 @@ HRESULT VtEngine::InvalidateSystem(_In_ const RECT* const /*prcDirtyClient*/)
 HRESULT VtEngine::InvalidateSelection(_In_reads_(cRectangles) const SMALL_RECT* const /*rgsrSelection*/,
                                       _In_ UINT const /*cRectangles*/)
 {
-    // Selection shouldn't be handled bt the VT Renderer Host, it should be 
+    // Selection shouldn't be handled bt the VT Renderer Host, it should be
     //      handled by the client.
 
     return S_OK;
@@ -64,15 +64,27 @@ HRESULT VtEngine::Invalidate(const SMALL_RECT* const psrRegion)
 // - pcoordCursor - the new position of the cursor
 // Return Value:
 // - S_OK
-HRESULT VtEngine::InvalidateCursor(const COORD* const /*pcoordCursor*/)
+HRESULT VtEngine::InvalidateCursor(const COORD* const pcoordCursor)
 {
+    // If we just inherited the cursor, we're going to get an InvalidateCursor
+    //      for both where the old cursor was, and where the new cursor is
+    //      (the inherited location). (See Cursor.cpp:Cursor::SetPosition)
+    // We should ignore the first one, but after that, if the client application
+    //      is moving the cursor around in the viewport, move our virtual top
+    //      up to meet their changes.
+    if (!_skipCursor && _virtualTop > pcoordCursor->Y)
+    {
+        _virtualTop = pcoordCursor->Y;
+    }
+    _skipCursor = false;
+
     _cursorMoved = true;
     return S_OK;
 }
 
 // Routine Description:
 // - Notifies to repaint everything.
-// - NOTE: Use sparingly. Only use when something that could affect the entire 
+// - NOTE: Use sparingly. Only use when something that could affect the entire
 //      frame simultaneously occurs.
 // Arguments:
 // - <none>
@@ -84,8 +96,8 @@ HRESULT VtEngine::InvalidateAll()
 }
 
 // Method Description:
-// - Notifies us that we're about to circle the buffer, giving us a chance to 
-//      force a repaint before the buffer contents are lost. The VT renderer 
+// - Notifies us that we're about to circle the buffer, giving us a chance to
+//      force a repaint before the buffer contents are lost. The VT renderer
 //      needs to be able to render all text before it's lost, so we return true.
 // Arguments:
 // - Recieves a bool indicating if we should force the repaint.
@@ -94,12 +106,17 @@ HRESULT VtEngine::InvalidateAll()
 HRESULT VtEngine::InvalidateCircling(_Out_ bool* const pForcePaint)
 {
     *pForcePaint = true;
+
+    // Keep track of the fact that we circled, we'll need to do some work on
+    //      end paint to specifically handle this.
+    _circled = true;
+
     return S_OK;
 }
 
 // Method Description:
-// - Notifies us that we're about to be torn down. This gives us a last chance 
-//      to force a repaint before the buffer contents are lost. The VT renderer 
+// - Notifies us that we're about to be torn down. This gives us a last chance
+//      to force a repaint before the buffer contents are lost. The VT renderer
 //      needs to be able to render all text before it's lost, so we return true.
 // Arguments:
 // - Recieves a bool indicating if we should force the repaint.
@@ -112,11 +129,11 @@ HRESULT VtEngine::PrepareForTeardown(_Out_ bool* const pForcePaint)
 }
 
 // Routine Description:
-// - Helper to combine the given rectangle into the invalid region to be 
+// - Helper to combine the given rectangle into the invalid region to be
 //      updated on the next paint
 // Expects EXCLUSIVE rectangles.
 // Arguments:
-// - invalid - A viewport containing the character region that should be 
+// - invalid - A viewport containing the character region that should be
 //      repainted on the next frame
 // Return Value:
 // - S_OK, else an appropriate HRESULT for failing to allocate or write.
--- src/terminal/adapter/InteractDispatch.cpp
+++ src/terminal/adapter/InteractDispatch.cpp
@@ -21,30 +21,30 @@ InteractDispatch::InteractDispatch(_In_ std::unique_ptr<ConGetSet> pConApi)
 }
 
 // Method Description:
-// - Writes a collection of input to the host. The new input is appended to the 
-//      end of the input buffer. 
-//  If Ctrl+C is written with this function, it will not trigger a Ctrl-C 
-//      interrupt in the client, but instead write a Ctrl+C to the input buffer 
+// - Writes a collection of input to the host. The new input is appended to the
+//      end of the input buffer.
+//  If Ctrl+C is written with this function, it will not trigger a Ctrl-C
+//      interrupt in the client, but instead write a Ctrl+C to the input buffer
 //      to be read by the client.
 // Arguments:
 // - inputEvents: a collection of IInputEvents
 // Return Value:
 // True if handled successfully. False otherwise.
-bool InteractDispatch::WriteInput(_In_ std::deque<std::unique_ptr<IInputEvent>>& inputEvents) 
+bool InteractDispatch::WriteInput(_In_ std::deque<std::unique_ptr<IInputEvent>>& inputEvents)
 {
     size_t dwWritten = 0;
     return !!_pConApi->WriteConsoleInputW(inputEvents, dwWritten);
 }
 
 // Method Description:
-// - Writes a Ctrl-C event to the host. The host will then decide what to do 
-//      with it, including potentially sending an interrupt to a client 
-//      application. 
+// - Writes a Ctrl-C event to the host. The host will then decide what to do
+//      with it, including potentially sending an interrupt to a client
+//      application.
 // Arguments:
 // <none>
 // Return Value:
 // True if handled successfully. False otherwise.
-bool InteractDispatch::WriteCtrlC() 
+bool InteractDispatch::WriteCtrlC()
 {
     KeyEvent key = KeyEvent(true, 1, 'C', 0, UNICODE_ETX, LEFT_CTRL_PRESSED);
     return !!_pConApi->PrivateWriteConsoleControlInput(key);
@@ -52,7 +52,7 @@ bool InteractDispatch::WriteCtrlC()
 
 //Method Description:
 // Window Manipulation - Performs a variety of actions relating to the window,
-//      such as moving the window position, resizing the window, querying 
+//      such as moving the window position, resizing the window, querying
 //      window state, forcing the window to repaint, etc.
 //  This is kept seperate from the output version, as there may be
 //      codes that are supported in one direction but not the other.
@@ -91,6 +91,80 @@ bool InteractDispatch::WindowManipulation(_In_ const DispatchCommon::WindowManip
         default:
             fSuccess = false;
             break;
+    }
+
+    return fSuccess;
+}
+
+//Method Description:
+// Move Cursor: Moves the cursor to the provided VT coordinates. This is the
+//      coordinate space where 1,1 is the top left cell of the viewport.
+//Arguments:
+// - row: The row to move the cursor to.
+// - col: The column to move the cursor to.
+// Return value:
+// True if we successfully moved the cursor to the given location.
+// False otherwise, including if given invalid coordinates (either component being 0)
+//  or if any API calls failed.
+bool InteractDispatch::MoveCursor(_In_ const unsigned int row, _In_ const unsigned int col)
+{
+    unsigned int uiRow = row;
+    unsigned int uiCol = col;
+
+    bool fSuccess = true;
+    // In VT, the origin is 1,1. For our array, it's 0,0. So subtract 1.
+    if (row != 0)
+    {
+        uiRow = row - 1;
+    }
+    else
+    {
+        // The parser should never return 0 (0 maps to 1), so this is a failure condition.
+        fSuccess = false;
+    }
+
+    if (col != 0)
+    {
+        uiCol = col - 1;
+    }
+    else
+    {
+        // The parser should never return 0 (0 maps to 1), so this is a failure condition.
+        fSuccess = false;
+    }
+
+    if (fSuccess)
+    {
+        // First retrieve some information about the buffer
+        CONSOLE_SCREEN_BUFFER_INFOEX csbiex = { 0 };
+        csbiex.cbSize = sizeof(CONSOLE_SCREEN_BUFFER_INFOEX);
+        fSuccess = !!_pConApi->GetConsoleScreenBufferInfoEx(&csbiex);
+
+        if (fSuccess)
+        {
+            COORD coordCursor = csbiex.dwCursorPosition;
+
+            // Safely convert the UINT positions we were given into shorts (which is the size the console deals with)
+            fSuccess = SUCCEEDED(UIntToShort(uiRow, &coordCursor.Y)) &&
+             SUCCEEDED(UIntToShort(uiCol, &coordCursor.X));
+
+            if (fSuccess)
+            {
+                // Set the line and column values as offsets from the viewport edge. Use safe math to prevent overflow.
+                fSuccess = SUCCEEDED(ShortAdd(coordCursor.Y, csbiex.srWindow.Top, &coordCursor.Y)) &&
+                    SUCCEEDED(ShortAdd(coordCursor.X, csbiex.srWindow.Left, &coordCursor.X));
+
+                if (fSuccess)
+                {
+                    // Apply boundary tests to ensure the cursor isn't outside the viewport rectangle.
+                    coordCursor.Y = max(min(coordCursor.Y, csbiex.srWindow.Bottom - 1), csbiex.srWindow.Top);
+                    coordCursor.X = max(min(coordCursor.X, csbiex.srWindow.Right - 1), csbiex.srWindow.Left);
+
+                    // Finally, attempt to set the adjusted cursor position back into the console.
+                    fSuccess = !!_pConApi->SetConsoleCursorPosition(coordCursor);
+                }
+            }
+        }
     }
 
     return fSuccess;
