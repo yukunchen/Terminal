abe5c67ac0dc8af4a3b6f4451ecaa3864237a92e
Austin Diviness <austdi@microsoft.com>
2018-03-08 19:21:53 -0800

    added nodiscard attribute to VtIo

--- src/host/VtIo.hpp
+++ src/host/VtIo.hpp
@@ -19,15 +19,20 @@ namespace Microsoft::Console::VirtualTerminal
     public:
         VtIo();
 
+        [[nodiscard]]
         HRESULT Initialize(_In_ const ConsoleArguments* const pArgs);
 
         bool IsUsingVt() const;
 
+        [[nodiscard]]
         HRESULT StartIfNeeded();
 
+        [[nodiscard]]
         static HRESULT ParseIoMode(_In_ const std::wstring& VtMode, _Out_ VtIoMode& ioMode);
 
+        [[nodiscard]]
         HRESULT SuppressResizeRepaint();
+        [[nodiscard]]
         HRESULT SetCursorPosition(_In_ const COORD coordCursor);
 
     private:
@@ -36,9 +41,13 @@ namespace Microsoft::Console::VirtualTerminal
         VtIoMode _IoMode;
         bool _lookingForCursorPosition;
 
+        [[nodiscard]]
         HRESULT _Initialize(_In_ const HANDLE InHandle, _In_ const HANDLE OutHandle, _In_ const std::wstring& VtMode);
+        [[nodiscard]]
         HRESULT _Initialize(_In_ const HANDLE InHandle, _In_ const HANDLE OutHandle, _In_ const std::wstring& VtMode, _In_opt_ HANDLE SignalHandle);
+        [[nodiscard]]
         HRESULT _Initialize(_In_ const std::wstring& InPipeName, _In_ const std::wstring& OutPipeName, _In_ const std::wstring& VtMode);
+        [[nodiscard]]
         HRESULT _Initialize(_In_ const std::wstring& InPipeName, _In_ const std::wstring& OutPipeName, _In_ const std::wstring& VtMode, _In_opt_ HANDLE SignalHandle);
 
         std::unique_ptr<Microsoft::Console::Render::VtEngine> _pVtRenderEngine;
