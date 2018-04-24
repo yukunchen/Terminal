025a7494e7bc059237c489b373887a04614f9728
Austin Diviness <austdi@microsoft.com>
2018-04-23 22:09:24 -0700

    removed knowledge of Ucs2CharRow from selection.cpp

--- src/host/selection.hpp
+++ src/host/selection.hpp
@@ -74,10 +74,10 @@ private:
 
     void _PaintSelection() const;
 
-    static void s_BisectSelection(const short sStringLength,
-                                  const COORD coordTargetPoint,
-                                  const SCREEN_INFORMATION& screenInfo,
-                                  _Inout_ SMALL_RECT* const pSmallRect);
+    static SMALL_RECT s_BisectSelection(const short sStringLength,
+                                        const COORD coordTargetPoint,
+                                        const SCREEN_INFORMATION& screenInfo,
+                                        const SMALL_RECT rect);
 
     void _CancelMarkSelection();
     void _CancelMouseSelection();
