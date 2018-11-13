35af925e1965126f6e9ec339913013298b823fca
Mike Griese <migrie@microsoft.com>
2018-11-13 10:19:10 -0800

    I guess it actually builds and links!
      In order to build a UWP:
      * Make sure to update pch.h with libraryincludes
      * make sure to add the libs as dependencies manually
      * make sure to add the AdditionalIncludeDirectories and Link.AdditionalDependencies

---
+++ src/cascadia/TerminalApp/MainPage.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "..\..\buffer\out\textBuffer.hpp"
+#include "../../renderer/inc/DummyRenderTarget.hpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::TerminalApp::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+        //CHAR_INFO fill;
+        //fill.Attributes = 0x7f;
+        COORD bufferSize;
+        bufferSize.X = 80;
+        bufferSize.Y = 9001;
+        UINT cursorSize = 12;
+        DummyRenderTarget _t;
+        TextAttribute attr{ 0x7f };
+        TextBuffer b(bufferSize, attr, cursorSize, _t);
+        b.Write({ L"F" });
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
