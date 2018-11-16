c5bd20bcb687bc2c40b3cacb7f708a24a98af61e
Mike Griese <migrie@microsoft.com>
2018-11-15 16:17:59 -0800

    Add a terminal connection?

---
+++ src/cascadia/TerminalConnection/ConptyConnection.cpp
@@ -0,0 +1,67 @@
+﻿#include "pch.h"
+#include "ConptyConnection.h"
+
+namespace winrt::TerminalConnection::implementation
+{
+    ConptyConnection::ConptyConnection(hstring const& commandline, uint32_t initialRows, uint32_t initialCols)
+    {
+        commandline;
+        initialRows;
+        initialCols;
+
+    }
+
+    winrt::event_token ConptyConnection::TerminalOutput(TerminalConnection::TerminalOutputEventArgs const& handler)
+    {
+        return _outputHandlers.add(handler);
+    }
+
+    void ConptyConnection::TerminalOutput(winrt::event_token const& token) noexcept
+    {
+        _outputHandlers.remove(token);
+    }
+
+    winrt::event_token ConptyConnection::TerminalDisconnected(TerminalConnection::TerminalDisconnectedEventArgs const& handler)
+    {
+        handler;
+
+        throw hresult_not_implemented();
+    }
+
+    void ConptyConnection::TerminalDisconnected(winrt::event_token const& token) noexcept
+    {
+        token;
+
+        // throw hresult_not_implemented();
+    }
+
+    void ConptyConnection::Start()
+    {
+
+        // When we recieve some data:
+        hstring outputFromConpty = L"hello world";
+        //TerminalOutputEventArgs args(outputFromConpty);
+        //_outputHandlers(*this, args);
+        _outputHandlers(outputFromConpty);
+    }
+
+    void ConptyConnection::WriteInput(hstring const& data)
+    {
+        data;
+
+        throw hresult_not_implemented();
+    }
+
+    void ConptyConnection::Resize(uint32_t rows, uint32_t columns)
+    {
+        rows;
+        columns;
+
+        throw hresult_not_implemented();
+    }
+
+    void ConptyConnection::Close()
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ src/cascadia/TerminalConnection/ConptyConnection.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "ConptyConnection.g.h"
+
+namespace winrt::TerminalConnection::implementation
+{
+    struct ConptyConnection : ConptyConnectionT<ConptyConnection>
+    {
+        ConptyConnection() = delete;
+        ConptyConnection(hstring const& commandline, uint32_t initialRows, uint32_t initialCols);
+
+        winrt::event_token TerminalOutput(TerminalConnection::TerminalOutputEventArgs const& handler);
+        void TerminalOutput(winrt::event_token const& token) noexcept;
+        winrt::event_token TerminalDisconnected(TerminalConnection::TerminalDisconnectedEventArgs const& handler);
+        void TerminalDisconnected(winrt::event_token const& token) noexcept;
+        void Start();
+        void WriteInput(hstring const& data);
+        void Resize(uint32_t rows, uint32_t columns);
+        void Close();
+
+    private:
+        winrt::event<TerminalConnection::TerminalOutputEventArgs> _outputHandlers;
+    };
+}
+
+namespace winrt::TerminalConnection::factory_implementation
+{
+    struct ConptyConnection : ConptyConnectionT<ConptyConnection, implementation::ConptyConnection>
+    {
+    };
+}
