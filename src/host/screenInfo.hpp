1c7c2e18d3475443f10781d524014453718ba882
Mike Griese (v2) <migrie@microsoft.com>
2018-12-14 15:39:34 +0000

    Merged PR 2696020: Re-initialize new lines in VT mode
    - fixes #17415310, for real this time. Last time I tried tying the re-initialization to _virtualBottom, beacuse I had been working a lot on that.
    
      This fix is much simpler instead - when we move the cursor below the viewport, and we're in VT mode, then re-initialize the new line we're on, just as if we were a terminal.
    - Add a test to cover this as well
    
    Cherry picked from !2669932
    
    Related work items: #17415310

--- src/host/screenInfo.hpp
+++ src/host/screenInfo.hpp
@@ -248,6 +248,8 @@ public:
     FontInfoDesired& GetDesiredFont() noexcept;
     const FontInfoDesired& GetDesiredFont() const noexcept;
 
+    void InitializeCursorRowAttributes();
+
 private:
     SCREEN_INFORMATION(_In_ IWindowMetrics *pMetrics,
                        _In_ IAccessibilityNotifier *pNotifier,
