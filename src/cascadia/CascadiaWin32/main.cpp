bc50d1529561d541fbd9d392df107364edc985c3
Mike Griese <migrie@microsoft.com>
2019-02-15 14:02:55 -0800

    Some cleanup of the sln
      I don't believe that this will pass CI, but it will help debug how to, that's for sure

--- src/cascadia/CascadiaWin32/main.cpp
+++ src/cascadia/CascadiaWin32/main.cpp
@@ -1,6 +1,5 @@
 #include "pch.h"
 #include "IslandWindow.h"
-#include <winrt/RuntimeComponent1.h>
 #include <winrt/TerminalControl.h>
 
 using namespace winrt;
@@ -8,59 +7,6 @@ using namespace Windows::UI;
 using namespace Windows::UI::Composition;
 using namespace Windows::UI::Xaml::Hosting;
 using namespace Windows::Foundation::Numerics;
-
-Windows::UI::Xaml::UIElement CreateDefaultContent()
-{
-    Windows::UI::Xaml::Media::AcrylicBrush acrylicBrush;
-    acrylicBrush.BackgroundSource(Windows::UI::Xaml::Media::AcrylicBackgroundSource::HostBackdrop);
-    acrylicBrush.TintOpacity(0.5);
-    acrylicBrush.TintColor(Windows::UI::Colors::Blue());
-    acrylicBrush.FallbackColor(Windows::UI::Colors::Cyan());
-
-    Windows::UI::Xaml::Controls::Grid container;
-    container.Margin(Windows::UI::Xaml::ThicknessHelper::FromLengths(100, 100, 100, 100));
-    container.Background(acrylicBrush);
-
-    Windows::UI::Xaml::Controls::Button b;
-    b.Width(600);
-    b.Height(60);
-    b.SetValue(Windows::UI::Xaml::FrameworkElement::VerticalAlignmentProperty(),
-        box_value(Windows::UI::Xaml::VerticalAlignment::Center));
-
-    b.SetValue(Windows::UI::Xaml::FrameworkElement::HorizontalAlignmentProperty(),
-        box_value(Windows::UI::Xaml::HorizontalAlignment::Center));
-    b.Foreground(Windows::UI::Xaml::Media::SolidColorBrush{ Windows::UI::Colors::White() });
-
-    Windows::UI::Xaml::Controls::TextBlock tb;
-    tb.Text(L"Hello Win32 love XAML and C++/WinRT xx");
-    b.Content(tb);
-    tb.FontSize(30.0f);
-    container.Children().Append(b);
-
-    Windows::UI::Xaml::Controls::TextBlock dpitb;
-    dpitb.Text(L"(p.s. high DPI just got much easier for win32 devs)");
-    dpitb.Foreground(Windows::UI::Xaml::Media::SolidColorBrush{ Windows::UI::Colors::White() });
-    dpitb.Margin(Windows::UI::Xaml::ThicknessHelper::FromLengths(10, 10, 10, 10));
-    dpitb.SetValue(Windows::UI::Xaml::FrameworkElement::VerticalAlignmentProperty(),
-        box_value(Windows::UI::Xaml::VerticalAlignment::Bottom));
-
-    dpitb.SetValue(Windows::UI::Xaml::FrameworkElement::HorizontalAlignmentProperty(),
-        box_value(Windows::UI::Xaml::HorizontalAlignment::Right));
-    container.Children().Append(dpitb);
-
-    // [=] is important, don't use [&]. [&] will capture b's address on the stack, which will be garbage
-    b.Click([=](auto, auto) {
-        winrt::RuntimeComponent1::Class term;
-        int a = term.MyProperty();
-        auto s = winrt::to_hstring(a);
-        Windows::UI::Xaml::Controls::TextBlock myTb{};
-        myTb.Text(s);
-        b.Content(myTb);
-    });
-    return container;
-}
-
-
 
 int __stdcall wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
 {
@@ -84,10 +30,7 @@ int __stdcall wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
 
     // Actually create some xaml content, and place it in the island
     // auto container = CreateDefaultContent();
-
-
     winrt::TerminalControl::TermControl t{};
-
     window.SetRootContent(t.GetControl());
 
     MSG message;
