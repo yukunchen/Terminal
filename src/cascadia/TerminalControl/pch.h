9ec6b0cfe8763008f110918c9a8ad1f13842dc33
Mike Griese <migrie@microsoft.com>
2019-02-21 14:13:04 -0800

    Try changing the type of these libs from "Universal Windows"
      This really does not work. I've gotten it close through some insanity of varibles here and there, but it doesn't launch.
      I believe this is because the wapproj no longer establishes that these dlls are needed by the app anymore.

--- src/cascadia/TerminalControl/pch.h
+++ src/cascadia/TerminalControl/pch.h
@@ -5,15 +5,23 @@
 
 #pragma once
 
+#include <LibraryIncludes.h>
+// This is inexplicable, but for whatever reason, cppwinrt conflicts with the
+//      SDK definition of this function, so the only fix is to undef it.
+// from WinBase.h
+// Windows::UI::Xaml::Media::Animation::IStoryboard::GetCurrentTime
+#ifdef GetCurrentTime
+#undef GetCurrentTime
+#endif
+
 #include <unknwn.h>
 #include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
 #include <winrt/windows.ui.core.h>
 #include <winrt/Windows.ui.input.h>
-#include <winrt/windows.ui.xaml.controls.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Windows.ui.xaml.media.h>
 #include <winrt/Windows.ui.xaml.input.h>
 
 #include <windows.ui.xaml.media.dxinterop.h>
 
-#include <LibraryIncludes.h>
