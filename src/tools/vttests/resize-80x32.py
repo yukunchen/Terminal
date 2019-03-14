1c200ed59a154f5c65079e965053f94f09081988
zadjii <zadjii@gmail.com>
2019-03-14 09:09:55 -0700

    Revert "Add ALL of my test scripts too, yolo"
    This reverts commit b89e3f0a3132446457a1d6440189c6fceb6c1652.

--- src/tools/vttests/resize-80x32.py
+++
@@ -1,9 +0,0 @@
-import sys
-import time # time.sleep is in seconds
-from common import *
-
-# $ python ~/vttests/name-of-file.py
-if __name__ == '__main__':
-    clear_all()
-    print('This is the VT Test template.')
-    csi('8;32;80t')
