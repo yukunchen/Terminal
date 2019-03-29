7b370f2491e9074087e6659f1226b7c87645bf91
Michael Niksa <miniksa@microsoft.com>
2019-03-29 23:16:09 +0000

    Merged PR 3070310: Add telemetry entrypoint and create provider for cascadia.
    Add telemetry entrypoint and create provider for cascadia.
    
    Related work items: #20987482

---
+++ src/cascadia/CascadiaWin32/telemetry.cpp
@@ -0,0 +1,18 @@
+/********************************************************
+*                                                       *
+*   Copyright (C) Microsoft. All rights reserved.       *
+*                                                       *
+********************************************************/
+
+#include "pch.h"
+
+#include "telemetry.hpp"
+
+#include "../../dep/telemetry/microsofttelemetry.h"
+
+TRACELOGGING_DEFINE_PROVIDER(
+    g_hTerminalWin32Provider,
+    "Microsoft.Windows.Terminal.Win32",
+    // {ef95331e-0ed7-55ba-39cf-c9d0d95499e0}
+    (0xef95331e, 0x0ed7, 0x55ba, 0x39, 0xcf, 0xc9, 0xd0, 0xd9, 0x54, 0x99, 0xe0),
+    TraceLoggingOptionMicrosoftTelemetry());
