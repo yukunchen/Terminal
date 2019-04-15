8f247bbdeec9c48c48dfa5d0ce1ce6c52c1129a9
Mike Griese <migrie@microsoft.com>
2019-04-15 12:16:56 -0500

    Revert "THis seems like a dead end"
    This reverts commit baa68b242b8eb74c5c542ea2a1a81c1c3435134d.

--- src/cascadia/CascadiaWin32/NonClientIslandWindow.h
+++ src/cascadia/CascadiaWin32/NonClientIslandWindow.h
@@ -43,5 +43,4 @@ private:
 
     void _HandleCreateWindow();
     void _HandleActivateWindow();
-    RECT GetMaxWindowRectInPixels(const RECT * const prcSuggested, _Out_opt_ UINT * pDpiSuggested);
 };
