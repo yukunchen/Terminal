37b77ef4b5c26aa0b21396285b604a8f3a1d94fb
Dustin Howett <duhowett@microsoft.com>
2019-04-30 19:55:52 +0000

    Merged PR 3209557: remove old C++ colortool
    remove old C++ colortool
    
    Related work items: #21343901

--- src/tools/colortool/schemes/do_colors.bat
+++
@@ -1,2 +0,0 @@
-rem @echo on
-%~dp0\colortool.exe %__BLACK__% %__DARK_BLUE__% %__DARK_GREEN__% %__DARK_CYAN__% %__DARK_RED__% %__DARK_MAGENTA__% %__DARK_YELLOW__% %__DARK_WHITE__% %__BRIGHT_BLACK__% %__BRIGHT_BLUE__% %__BRIGHT_GREEN__% %__BRIGHT_CYAN__% %__BRIGHT_RED__% %__BRIGHT_MAGENTA__% %__BRIGHT_YELLOW__% %__WHITE__%
